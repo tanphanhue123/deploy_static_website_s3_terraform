bc0b18b997152d62587184e2c8322cf6a1c3d4e0
EC2 Default User <ec2-user@ip-172-31-16-66.ap-southeast-1.compute.internal>
2023-03-03 12:57:05 +0000

    done

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.44.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "static" {
+  bucket        = "tanphan1234"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "static" {
+  bucket = aws_s3_bucket.static.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "static" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static.arn}/*"]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = data.aws_iam_policy_document.static.json
+}   
